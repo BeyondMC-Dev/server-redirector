60b3f63f54b44e080909c1f07ef14bcb6fd2889b
Saturn <github.vcnmd@passinbox.com>
2026-01-12 17:42:06 +0000

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -3,5 +3,5 @@ import { transferPlayer } from "@minecraft/server-admin";
 
 world.afterEvents.playerSpawn.subscribe(event => {
     const player = event.player;
-    transferPlayer(player, { hostname: "108.175.12.25", port: 19132 });
+    transferPlayer(player, { hostname: "beyondmc.net", port: 19132 });
 });
